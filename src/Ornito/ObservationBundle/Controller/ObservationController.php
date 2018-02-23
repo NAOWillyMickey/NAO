a2e8522bf234d8bddd634af53caddb81d91e31ea
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2018-02-23 17:33:41 +0100

    add view and path

--- src/Ornito/ObservationBundle/Controller/ObservationController.php
+++ src/Ornito/ObservationBundle/Controller/ObservationController.php
@@ -3,11 +3,32 @@
 namespace Ornito\ObservationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ObservationController extends Controller
 {
     public function indexAction()
     {
         return $this->render('OrnitoObservationBundle:Observation:index.html.twig');
+    }
+
+    public function viewAction($id)
+    {
+        return $this->render('OrnitoObservationBundle:Observation:view.html.twig');
+    }
+
+    public function addAction(Request $request)
+    {
+        return $this->render('OrnitoObservationBundle:Observation:add.html.twig');
+    }
+
+    public function editAction(Request $request, $id)
+    {
+        return $this->render('OrnitoObservationBundle:Observation:edit.html.twig');
+    }
+
+    public function deleteAction(Request $request, $id)
+    {
+        return $this->render('OrnitoObservationBundle:Observation:delete.html.twig');
     }
 }
