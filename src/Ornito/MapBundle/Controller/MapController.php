7d6a0c2f51befc323ab0bb69e6b259bcee6540df
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2018-02-27 16:45:04 +0100

    improve indexAction and add filter function

--- src/Ornito/MapBundle/Controller/MapController.php
+++ src/Ornito/MapBundle/Controller/MapController.php
@@ -3,11 +3,46 @@
 namespace Ornito\MapBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class MapController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('OrnitoMapBundle:Map:index.html.twig');
+        $repository = $this
+            ->getDoctrine()
+            ->getManager()
+            ->getRepository('OrnitoTaxrefBundle:Species');
+
+        $req = $repository->findColumns();
+        $ordreList = $this->filter(array_column($req, 'ordre'));
+        $familyList = $this->filter(array_column($req, 'family'));
+        return $this->render('OrnitoMapBundle:Map:index.html.twig', array(
+            'ordreList' => $ordreList,
+            'familyList' => $familyList
+        ));
+    }
+
+    public function filter($tab)
+    {
+        $list = array();
+        foreach ($tab as $value) {
+            if (!in_array($value, $list)) {
+                $list[] = $value;
+            }
+        }
+        return $list;
+    }
+
+    public function searchAction(Request $request)
+    {
+        if ($request->isXmlHttpRequest()) {
+            $repo = $this->getDoctrine()->getManager()->getRepository('OrnitoTaxrefBundle:Species');
+            //$reqName = 'findBy'.$column;
+            //$req = $repo->$reqName($value);
+            $req = $repo->findAll();
+            return new JsonResponse(array('req' => $req));
+        }
     }
 }
