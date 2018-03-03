f5109c70602b7d957e106406a2bde8e15a5dd413
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2018-03-03 18:30:02 +0100

    really improve the speed to get data for the select form

--- web/assets/js/select_form.js
+++ web/assets/js/select_form.js
@@ -7,50 +7,75 @@ $(function () {
     var scientElt = $('#ref-nomScientifique');
     var vernElt = $('#ref-nomVernaculaire');
     var pathElt = $('#path');
+    var formElt = $('#birdCardSearh');
+    var speciesList = null;
+
+    $(formElt).hide();
+
+    $.ajax({
+        type: 'GET',
+        url: pathElt.attr("href"),
+        dataType : "json",
+        timeout: 12000,
+        success: function(response) {
+            speciesList = response.json;
+            $(formElt).show(1000,"linear");
+        },
+        error: function() {
+            alert('La requête n\'a pas abouti'); }
+    });
 
     $('select').change(function() {
         var value = $(this).val();
         var name = $(this).attr('name');
-        $.ajax({
-            type: 'GET',
-            url: pathElt.attr("href")+'/'+name+'/'+value,
-            dataType : "json",
-            timeout: 8000,
-            success: function(response) {
-                if (name === 'ordre') {
-                    return amendListWithOrdreName(response);
-                } else if (name === 'family') {
-                    return amendListWithFamilyName(response);
-                } else if (name === 'scientificName') {
-                    return amendListWithScientificName(response);
-                } else {
-                    return amendListWithVernName(response);
-                }
-            },
-            error: function() {
-                alert('La requête n\'a pas abouti'); }
-        });
+        if (name === 'ordre') {
+            return amendListWithOrdreName(value);
+        } else if (name === 'family') {
+            return amendListWithFamilyName(value);
+        } else if (name === 'scientificName') {
+            return amendListWithScientificName(value);
+        } else {
+            return amendListWithVernName(value);
+        }
     });
 
-    function amendListWithOrdreName(response) {
-        fillListFamily(response);
-        fillListScientific(response);
-        fillListVern(response);
+    function amendListWithOrdreName(value) {
+        var result = speciesList.filter(check);
+        function check(obj) {
+            if (obj.species_ordre === value) {
+                return true;
+            }
+        }
+        fillListFamily(result);
+        fillListScientific(result);
+        fillListVern(result);
     }
 
-    function amendListWithFamilyName(response) {
-        var ordre = response.json[0].ordre;
+    function amendListWithFamilyName(value) {
+        var result = speciesList.filter(check);
+        function check(obj) {
+            if (obj.species_family === value) {
+                return true;
+            }
+        }
+        var ordre = result[0]['species_ordre'];
         if (ordre !== ordreElt.val()) {
             $('option[value="' + ordre + '"]').prop('selected', true);
         }
-        fillListScientific(response);
-        fillListVern(response);
+        fillListScientific(result);
+        fillListVern(result);
     }
 
-    function amendListWithScientificName(response) {
-        var ordre = response.json[0].ordre;
-        var family = response.json[0].family;
-        var vern = response.json[0].vernFr;
+    function amendListWithScientificName(value) {
+        var result = speciesList.filter(check);
+        function check(obj) {
+            if (obj.species_scientificName === value) {
+                return true;
+            }
+        }
+        var ordre = result[0]['species_ordre'];
+        var family = result[0]['species_family'];
+        var vern = result[0]['species_vernFr'];
         if (vern === "") {
             $('option[value="noVern"]').prop('selected', true);
         } else {
@@ -60,10 +85,16 @@ $(function () {
         $('option[value="'+family+'"]').prop('selected', true);
     }
 
-    function amendListWithVernName(response) {
-        var ordre = response.json[0].ordre;
-        var family = response.json[0].family;
-        var scientificName = response.json[0].scientificName;
+    function amendListWithVernName(value) {
+        var result = speciesList.filter(check);
+        function check(obj) {
+            if (obj.species_vernFr === value) {
+                return true;
+            }
+        }
+        var ordre = result[0]['species_ordre'];
+        var family = result[0]['species_family'];
+        var scientificName = result[0]['species_scientificName'];
         if (ordre !== ordreElt.val()) {
             $('option[value="' + ordre + '"]').prop('selected', true);
         }
@@ -73,37 +104,37 @@ $(function () {
         $('option[value="'+scientificName+'"]').prop('selected', true);
     }
 
-    function fillListFamily(response) {
+    function fillListFamily(result) {
         $(familyElt).empty().append('<option disabled selected>----------</option>');
         var tableau = [];
-        $.each(response.json, function(key, value){
-            if (jQuery.inArray(value.family, tableau) === -1) {
-                tableau.push(value.family);
-                var $toAdd = '<option value="' + value.family + '">' + value.family + '</option>';
+        $.each(result, function(key, bird){
+            if (jQuery.inArray(bird.species_family, tableau) === -1) {
+                tableau.push(bird.species_family);
+                var $toAdd = '<option value="' + bird.species_family + '">' + bird.species_family + '</option>';
                 $(familyElt).append($toAdd);
             }
         })
     }
 
-    function fillListScientific(response) {
+    function fillListScientific(result) {
         $(scientElt).empty().append('<option disabled selected>----------</option>');
         var tableau = [];
-        $.each(response.json, function(key, value){
-            if (jQuery.inArray(value.scientificName, tableau) === -1) {
-                tableau.push(value.scientificName);
-                var $toAdd = '<option value="' + value.scientificName + '">' + value.scientificName + '</option>';
+        $.each(result, function(key, bird){
+            if (jQuery.inArray(bird.species_scientificName, tableau) === -1) {
+                tableau.push(bird.species_scientificName);
+                var $toAdd = '<option value="' + bird.species_scientificName + '">' + bird.species_scientificName + '</option>';
                 $(scientElt).append($toAdd);
             }
         })
     }
 
-    function fillListVern(response) {
+    function fillListVern(result) {
         $(vernElt).empty().append('<option disabled selected value="noVern">----------</option>');
         var tableau = [];
-        $.each(response.json, function(key, value){
-            if (jQuery.inArray(value.vernFr, tableau) === -1 && value.vernFr !== "") {
-                tableau.push(value.vernFr);
-                var $toAdd = '<option value="' + value.vernFr + '">' + value.vernFr + '</option>';
+        $.each(result, function(key, bird){
+            if (jQuery.inArray(bird.species_vernFr, tableau) === -1 && bird.species_vernFr !== "") {
+                tableau.push(bird.species_vernFr);
+                var $toAdd = '<option value="' + bird.species_vernFr + '">' + bird.species_vernFr + '</option>';
                 $(vernElt).append($toAdd);
             }
         })
