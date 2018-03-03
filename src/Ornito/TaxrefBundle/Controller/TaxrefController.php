9b4e7a0603673818af4d3248dde2c297a5d9f049
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2018-03-03 09:55:12 +0100

    fill findAction & amend indexAction when a request is POST

--- src/Ornito/TaxrefBundle/Controller/TaxrefController.php
+++ src/Ornito/TaxrefBundle/Controller/TaxrefController.php
@@ -5,15 +5,30 @@ namespace Ornito\TaxrefBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class TaxrefController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $birds = null;
         $repository = $this
             ->getDoctrine()
             ->getManager()
             ->getRepository('OrnitoTaxrefBundle:Species');
+
+        if ($request->isMethod('POST')) {
+            $data = $request->request;
+            $tab = [];
+            foreach ($data as $key => $value) {
+                array_push($tab, $key, $value);
+            }
+            for ($i = 0; $i < 2; $i++) {
+                array_pop($tab);
+            }
+            $output = array_slice($tab, -2);
+            $birds = $repository->mySelectList($output[0], $output[1]);
+        }
 
         $ordreList = $repository->getList('ordre');
         $familyList = $repository->getList('family');
@@ -26,12 +41,22 @@ class TaxrefController extends Controller
             'familyList' => $familyList,
             'scientificList' => $scientificList,
             'vernList' => $vernList,
+            'birds' => $birds,
         ));
     }
 
     public function findAction($id)
     {
-        return $this->render('OrnitoTaxrefBundle:Taxref:find.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $species = $em->getRepository('OrnitoTaxrefBundle:Species')->find($id);
+
+        if (null === $species) {
+            throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+        }
+
+        return $this->render('OrnitoTaxrefBundle:Taxref:find.html.twig', array(
+            'bird' => $species,
+        ));
     }
 
     public function searchAction(Request $request, $name, $value)
