539a8c175b8b7cdacce9d56a0bc1003c9aafcd4b
Mickey3d <mickey.welensky@gmail.com>
2018-03-04 13:30:25 +0100

    UserBundle Update
    - new controllers for profile access ressources and users admin
    - Admin Hambuger and sidebar menu implemented in base.html.twig
    - admin/user route added
    - correction in template for directory name (first comment line)

---
+++ src/Ornito/UserBundle/Controller/ProfileController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ornito\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ProfileController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+
+    }
+
+
+}
