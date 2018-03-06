373903fdbdaba9a4c09145617e29f85dc5ed102f
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2018-03-06 13:45:20 +0100

    fill addAction function

--- src/Ornito/ObservationBundle/Controller/ObservationController.php
+++ src/Ornito/ObservationBundle/Controller/ObservationController.php
@@ -2,6 +2,8 @@
 
 namespace Ornito\ObservationBundle\Controller;
 
+use Ornito\ObservationBundle\Entity\Watching;
+use Ornito\ObservationBundle\Form\WatchingType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -19,7 +21,21 @@ class ObservationController extends Controller
 
     public function addAction(Request $request)
     {
-        return $this->render('OrnitoObservationBundle:Observation:add.html.twig');
+        $watching = new Watching($this->getUser());
+        $form = $this->createForm(WatchingType::class, $watching);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($watching);
+            $em->flush();
+
+            $this->redirectToRoute('ornito_observation_view', array(
+                'id' => $watching->getId(),
+            ));
+        }
+        return $this->render('OrnitoObservationBundle:Observation:add.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 
     public function editAction(Request $request, $id)
