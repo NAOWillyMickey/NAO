8ed97834851dec5f89bb3d03b0831a7c5175d720
Mickey3d <mickey.welensky@gmail.com>
2018-03-06 16:08:10 +0100

    Label Esthetic patch

--- src/Ornito/UserBundle/Form/AvatarType.php
+++ src/Ornito/UserBundle/Form/AvatarType.php
@@ -15,7 +15,7 @@ class AvatarType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('file', FileType::class);
+        ->add('file', FileType::class, array('label' => '°'));
     }
     /**
      * {@inheritdoc}
