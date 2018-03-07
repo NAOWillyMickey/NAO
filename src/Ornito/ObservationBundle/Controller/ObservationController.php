2a4af90ffdd3cd947765c64e4b3975cab5b229fa
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2018-03-07 19:53:53 +0100

    add delete function for observations

--- src/Ornito/ObservationBundle/Controller/ObservationController.php
+++ src/Ornito/ObservationBundle/Controller/ObservationController.php
@@ -32,23 +32,33 @@ class ObservationController extends Controller
 
     public function addAction(Request $request)
     {
-        $watching = new Watching($this->getUser());
-        $form = $this->createForm(WatchingType::class, $watching);
+        $user = $this->getUser();
+        if (null === $user) {
+            $request->getSession()->getFlashBag()->add('info', 'Connectez-vous ou créez un compte pour ajouter une observation');
+            return $this->redirectToRoute('fos_user_registration_register');
+        } else {
+            $watching = new Watching($this->getUser());
+            $form = $this->createForm(WatchingType::class, $watching);
 
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($watching);
-            $em->flush();
+            if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($watching);
+                $em->flush();
 
-            $request->getSession()->getFlashBag()->add('success', 'Annonce bien enregistrée.');
+                $request->getSession()->getFlashBag()->add('success', 'Annonce bien enregistrée.');
 
-            $this->redirectToRoute('ornito_observation_view', array(
-                'id' => $watching->getId(),
-            ));
+                return $this->redirectToRoute('ornito_observation_view', array(
+                    'id' => $watching->getId(),
+                ));
+
+            } else {
+
+                return $this->render('OrnitoObservationBundle:Observation:add.html.twig', array(
+                    'form' => $form->createView(),
+                ));
+            }
         }
-        return $this->render('OrnitoObservationBundle:Observation:add.html.twig', array(
-            'form' => $form->createView(),
-        ));
+
     }
 
     public function editAction(Request $request, $id)
@@ -58,6 +68,16 @@ class ObservationController extends Controller
 
     public function deleteAction(Request $request, $id)
     {
-        return $this->render('OrnitoObservationBundle:Observation:delete.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $watching = $em->getRepository('OrnitoObservationBundle:Watching')->find($id);
+
+        if ($watching === null) {
+            throw new NotFoundHttpException('L\'observation d\'id ' . $id . ' n\'existe pas!');
+        }
+
+        $em->remove($watching);
+        $em->flush();
+        $request->getSession()->getFlashBag()->add('success', 'L\'observation a bien été supprimée.');
+        return $this->redirectToRoute('fos_user_profile_show');
     }
 }
