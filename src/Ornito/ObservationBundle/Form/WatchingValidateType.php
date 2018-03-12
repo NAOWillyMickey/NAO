87291edc79475930112c5841a49d102dc9bf048b
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2018-03-12 17:35:08 +0100

    fill missing trans elements

---
+++ src/Ornito/ObservationBundle/Form/WatchingValidateType.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilfriedcottineau
+ * Date: 12/03/2018
+ * Time: 10:26
+ */
+
+namespace Ornito\ObservationBundle\Form;
+
+
+class WatchingValidateType
+{
+
+}
