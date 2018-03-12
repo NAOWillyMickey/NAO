55d4a037fec627694ef87648a5ed0cefc628851f
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2018-03-12 17:37:18 +0100

    create validate form

--- src/Ornito/ObservationBundle/Form/WatchingValidateType.php
+++ src/Ornito/ObservationBundle/Form/WatchingValidateType.php
@@ -1,15 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: wilfriedcottineau
- * Date: 12/03/2018
- * Time: 10:26
- */
 
 namespace Ornito\ObservationBundle\Form;
 
+use Ornito\ObservationBundle\Entity\Watching;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
 
-class WatchingValidateType
+class WatchingValidateType extends AbstractType
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->remove('description')
+            ->remove('latitude')
+            ->remove('longitude')
+            ->remove('title')
+            ->remove('image')
+            ->remove('species')
+            ->add('validateStatus', ChoiceType::class, array(
+                'choices' => array(
+                    'Valider' => Watching::ATTESTED,
+                    'Refuser' => Watching::REJECTED,
+                ),
+                'expanded' => true,
+                'multiple' => false,
+            ))
+        ;
+    }
 
+    public function getParent()
+    {
+        return WatchingType::class;
+    }
 }
