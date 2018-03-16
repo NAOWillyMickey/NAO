4e5a83aab04c686defe348bf97a07a0e1590c668
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2018-03-16 13:46:57 +0100

    fix a lack of orthography

---
+++ src/Ornito/MainBundle/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilfriedcottineau
+ * Date: 15/03/2018
+ * Time: 19:15
+ */
+
+namespace Ornito\MainBundle\EventSubscriber;
+
+
+class LocaleSubscriber
+{
+
+}
